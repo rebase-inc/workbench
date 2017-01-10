c97d95556e69ead27ed3e3e3412da9abcd6fb11a
Andrew Millspaugh <millspaugh.andrew@gmail.com>
2017-01-10 00:55:31 -0800

    Fixed progress bars to use job meta data

--- scanner/scan.py
+++ scanner/scan.py
@@ -17,48 +17,46 @@ PASSWORD = os.environ['GITHUB_CRAWLER_PASSWORD']
 
 def scan_public_users(*github_ids, show_progress = True):
     connection = StrictRedis(host = 'redis', port = 6379)
-    crawler_queue = Queue('public_github_scanner', connection = connection)
-    population_queue = Queue('population_analyzer', connection = connection)
+    crawler_queue = Queue('public_github_scanner', connection = connection, default_timeout = 3600)
     jobs = []
     for github_id in github_ids:
         access_token = authgen.create_github_access_token(USERNAME, PASSWORD, 'public scan {}'.format(github_id))
-        scan_repos = crawler_queue.enqueue_call(func = 'scanner.scan_all_repos', args = (access_token, github_id), timeout = 3600, result_ttl=86400)
+        scan_repos = crawler_queue.enqueue_call(func = 'scanner.scan_all_repos', args = (access_token, github_id), result_ttl=86400, meta = {'remaining': [], 'finished': []})
         delete_token = crawler_queue.enqueue_call(func = 'scanner.delete_github_access_token', args = (USERNAME, PASSWORD, access_token), timeout = 60, depends_on = scan_repos)
         jobs.append((scan_repos, delete_token))
     if show_progress:
-        show_scan_progress(*jobs)
+        show_progress_bars(*jobs)
     return jobs
 
-def show_scan_progress(*scan_jobs):
+def show_progress_bars(*jobs):
     from IPython.lib import backgroundjobs
     bgjobs = backgroundjobs.BackgroundJobManager()
-    for scan_job in reversed(scan_jobs): # ipywidgets displays newest things on top
-        #_show_scan_progress(scan_job)
-        bgjobs.new(_show_scan_progress, scan_job)
+    for job in reversed(jobs): # ipywidgets displays newest things on top
+        bgjobs.new(show_progress_bar, *job)
+        #show_progress_bar(*job)
 
-def _show_scan_progress(scan_job):
+def show_progress_bar(scan_job, delete_job):
     from ipywidgets import FloatProgress, Label, VBox, HBox
     from IPython.display import display
     progress_bars = []
-    label = Label('GitHub user "{}"'.format(scan_job[0].args[-1]))
-    progress_bars.append(HBox([FloatProgress(value=0.0, min=0.0, max=1.0, step=0.01, bar_style='info'), Label('Scan user repositories')]))
-    progress_bars.append(HBox([FloatProgress(value=0.0, min=0.0, max=1.0, step=0.01, bar_style='info'), Label('Delete authentication token')]))
-    box = VBox([label] + progress_bars)
+    scan_progress = FloatProgress(value=0.0, min=0.0, max=1.0, step=0.01, bar_style='info')
+    scan_label = Label('Scan user repositories')
+    delete_progress = FloatProgress(value=0.0, min=0.0, max=1.0, step=0.01, bar_style='info')
+    delete_label = Label('Delete authentication token')
+    box = VBox([
+        Label('GitHub user "{}"'.format(scan_job.args[-1])),
+        HBox([ scan_progress, scan_label ]),
+        HBox([ delete_progress, delete_label ])
+        ])
     display(box)
-    finished = 0
+    bar_styles = {'queued': 'info', 'started': 'info', 'deferred': 'warning', 'failed': 'danger', 'finished': 'success' }
     while True:
-        for job, progress_bar in zip(scan_job, progress_bars):
-            if job.status in ['queued', 'started', 'deferred']:
-                time.sleep(5)
-                continue
-            elif job.status == 'finished':
-                finished += 1
-                progress_bar.children[0].value = 1.0
-                progress_bar.children[0].bar_style = 'success'
-                print(job.result)
-            else:
-                finished += 1
-                progress_bar.children[0].value = 1.0
-                progress_bar.children[0].bar_style = 'danger'
-        if finished == len(scan_job):
+        scan_job.refresh() # get new meta data
+        scan_progress.value = len(scan_job.meta['finished']) / float(len(scan_job.meta['finished'] + scan_job.meta['remaining']) or 1.0)
+        scan_progress.bar_style = bar_styles[scan_job.status]
+        delete_progress.value = 0.0 if delete_job.status in ['queued', 'started', 'deferred'] else 1.0
+        delete_progress.bar_style = bar_styles[delete_job.status]
+        if scan_progress.value == delete_progress.value == 1.0 or scan_job.status == 'failed':
+            time.sleep(2) # enough time for graphics to update
             break
+        time.sleep(2)
